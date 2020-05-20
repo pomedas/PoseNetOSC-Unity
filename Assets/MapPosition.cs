1d45b17b5fa3e2087abbcf70af2767b90a2b883c
Pedro Omedas <pomedas.specs@gmail.com>
2020-05-20 19:27:33 +0200

    Added FastIK asset Modified Avatar of FastIK project Added representation of all PoseNet joints Added line renderer to draw the linews between joints

---
+++ Assets/MapPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPosition : MonoBehaviour
+{
+    public GameObject TargetObject1;
+    public GameObject TargetObject2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = (TargetObject1.transform.position.x+ TargetObject2.transform.position.x) / 2.0f;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
--- Assets/TrackingReceiver.cs
+++ Assets/TrackingReceiver.cs
@@ -9,7 +9,18 @@ public class TrackingReceiver : MonoBehaviour
 {
     //GameObjects to be controlled with Posenet
     public GameObject nose;
-    public GameObject wristR;
+    public GameObject leftShoulder;
+    public GameObject rightShoulder;
+    public GameObject leftElbow;
+    public GameObject rightElbow;
+    public GameObject leftWrist;
+    public GameObject rightWrist;
+    public GameObject leftHip;
+    public GameObject rightHip;
+    public GameObject leftKnee;
+    public GameObject rightKnee;
+    public GameObject leftAnkle;
+    public GameObject rightAnkle;
 
     //OSC Variables
     private OSCReceiver _receiver;
@@ -17,6 +28,12 @@ public class TrackingReceiver : MonoBehaviour
 
     //Dictionary to store pose data
     public Dictionary<string, Vector3> pose = new Dictionary<string, Vector3>();
+
+    //The number of lines
+    int lengthOfLineRenderer = 18;
+
+    //Set active the line renderer
+    public bool lineRenderOn = false; 
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -26,6 +43,16 @@ public class TrackingReceiver : MonoBehaviour
 
         //Initialize pose
         StartPose();
+
+        //Creates the lines
+        if (lineRenderOn)
+        {
+            LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            lineRenderer.widthMultiplier = 2f;
+            lineRenderer.positionCount = lengthOfLineRenderer;
+        }
+
     }
 
     void StartOSCReceiver() {
@@ -41,15 +68,65 @@ public class TrackingReceiver : MonoBehaviour
 
     void StartPose() {
         pose.Add("nose", Vector3.zero);
+        pose.Add("leftShoulder", Vector3.zero);
+        pose.Add("rightShoulder", Vector3.zero);
+        pose.Add("leftElbow", Vector3.zero);
+        pose.Add("rightElbow", Vector3.zero);
+        pose.Add("leftWrist", Vector3.zero);
         pose.Add("rightWrist", Vector3.zero);
+        pose.Add("leftHip", Vector3.zero);
+        pose.Add("rightHip", Vector3.zero);
+        pose.Add("leftKnee", Vector3.zero);
+        pose.Add("rightKnee", Vector3.zero);
+        pose.Add("leftAnkle", Vector3.zero);
+        pose.Add("rightAnkle", Vector3.zero);
     }
     
 
     // Update is called once per frame
     void Update()
     {
+        //Update GameObjects positions that represents the joins in PoseNet
+        //Note: these mappings are mirror, to obtain a correct side position when you see the character from the back
         nose.transform.position = pose["nose"];
-        wristR.transform.position = pose["rightWrist"];
+        leftShoulder.transform.position = pose["rightShoulder"];
+        rightShoulder.transform.position = pose["leftShoulder"];
+        leftElbow.transform.position = pose["rightElbow"];
+        rightElbow.transform.position = pose["leftElbow"];
+        leftWrist.transform.position = pose["rightWrist"];
+        rightWrist.transform.position = pose["leftWrist"];
+        leftHip.transform.position = pose["rightHip"];
+        rightHip.transform.position = pose["leftHip"];
+        leftKnee.transform.position = pose["rightKnee"];
+        rightKnee.transform.position = pose["leftKnee"];
+        leftAnkle.transform.position = pose["rightAnkle"];
+        rightAnkle.transform.position = pose["leftAnkle"];
+
+        //If lineRenderOn = true draw the lines
+        if (lineRenderOn)
+        {
+            //Update lines
+            LineRenderer lineRenderer = GetComponent<LineRenderer>();
+            var points = new Vector3[lengthOfLineRenderer];
+            lineRenderer.SetPosition(0, leftWrist.transform.position);
+            lineRenderer.SetPosition(1, leftElbow.transform.position);
+            lineRenderer.SetPosition(2, leftShoulder.transform.position);
+            lineRenderer.SetPosition(3, leftHip.transform.position);
+            lineRenderer.SetPosition(4, leftKnee.transform.position);
+            lineRenderer.SetPosition(5, leftAnkle.transform.position);
+            lineRenderer.SetPosition(6, leftKnee.transform.position);
+            lineRenderer.SetPosition(7, leftHip.transform.position);
+            lineRenderer.SetPosition(8, rightHip.transform.position);
+            lineRenderer.SetPosition(9, rightKnee.transform.position);
+            lineRenderer.SetPosition(10, rightAnkle.transform.position);
+            lineRenderer.SetPosition(11, rightKnee.transform.position);
+            lineRenderer.SetPosition(12, rightHip.transform.position);
+            lineRenderer.SetPosition(13, rightShoulder.transform.position);
+            lineRenderer.SetPosition(14, leftShoulder.transform.position);
+            lineRenderer.SetPosition(15, rightShoulder.transform.position);
+            lineRenderer.SetPosition(16, rightElbow.transform.position);
+            lineRenderer.SetPosition(17, rightWrist.transform.position);
+        }
 
     }
 
@@ -76,4 +153,5 @@ public class TrackingReceiver : MonoBehaviour
         }
 
     }
+
 }
